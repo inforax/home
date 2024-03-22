3f4602d040b8bf4da063683e6b80bf8c73ee4562
KayzerCode <zlo.alien@gmail.com>
2024-03-22 16:52:53 +0200

    js include

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,13 +5,48 @@ console.log(categories);
 sidebar.innerHTML = `
 <!-- Right Sidebar -->
 <div class="col-md-3">
- <div
-  w3-include-html="https://inforax.com/cats.html"></div>
- <div
-  w3-include-html="https://inforax.com/latest.html"></div>
- <div class="plugin plugin-tags"></div>
- <div
-  w3-include-html="https://inforax.com/about.html"></div>
- <script>w3IncludeHTML();</script>
+
+<div class="plugin plugin-categories">
+ <h2 class="plugin-label">Categories</h2>
+ <div class="plugin-content">
+  <ul>
+   <li><a href="https://inforax.com/category/general">General (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/startup">Startup (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/ideas">Идеи (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/projects">Проекты (2)</a>
+   </li>
+   <li><a href="https://inforax.com/category/social">Соцсети (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/tasks">Задачки(6)</a>
+   </li>
+   <li><a href="https://inforax.com/category/telegramm">Telegramm (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/usefultools">Инструменты (21)</a>
+   </li>
+  </ul>
+ </div>
 </div>
+
+
+<div class="plugin plugin-navigation">
+ <h2 class="plugin-label">Latest</h2>
+ <div class="plugin-content">
+  <ul>
+   <li><a href="https://inforax.com/seed-prase">Основы криптографии? Что такое исходная фраза?</a></li>
+   <li><a href="https://inforax.com/generate-ssh-key-cpanel">Как создать ssh key в панели CPanel</a></li>
+   <li><a href="https://inforax.com/exim">Полезные действия Exim</a></li>
+   <li><a href="https://inforax.com/zip-ssh">ZIP SSH - как запаковать все файлы в текущем каталоге</a></li>
+   <li><a href="https://inforax.com/osnova-paradigmy">Основа парадигмы</a></li>
+  </ul>
+ </div>
+</div>
+
+<div class="plugin plugin-about">
+ <h2 class="plugin-label">Contact:</h2>
+ <div class="plugin-content">Tlg:@RedBlackTg</div>
+</div>
+
 </div>`;
