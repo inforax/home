39a2662b48daccd5e4ce1a688313edcc1a2ac53b
KayzerCode <zlo.alien@gmail.com>
2024-03-22 16:39:03 +0200

    js include

---
+++ components/categories.js
@@ -0,0 +1,26 @@
+export default function categories() {
+ return `
+<div class="plugin plugin-categories">
+ <h2 class="plugin-label">Categories</h2>
+ <div class="plugin-content">
+  <ul>
+   <li><a href="https://inforax.com/category/general">General (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/startup">Startup (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/ideas">Идеи (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/projects">Проекты (2)</a>
+   </li>
+   <li><a href="https://inforax.com/category/social">Соцсети (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/tasks">Задачки(6)</a>
+   </li>
+   <li><a href="https://inforax.com/category/telegramm">Telegramm (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/usefultools">Инструменты (21)</a>
+   </li>
+  </ul>
+ </div>
+</div>
+`};
