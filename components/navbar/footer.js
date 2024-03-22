3c016baee5b5306ee8ed95fedc4df6f535536c97
KayzerCode <zlo.alien@gmail.com>
2024-03-22 16:07:14 +0200

    js include

---
+++ components/navbar/footer.js
@@ -0,0 +1,8 @@
+
+let component = document.getElementById("footer");
+component.innerHTML = `
+ <div class="container">
+  <p
+   class="m-0 text-center text-white text-uppercase">Copyright
+   © 2020</p>
+ </div>`;
