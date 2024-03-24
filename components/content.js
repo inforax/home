984595cca2777ab11c2c92eb046f9eb31194b9d9
KayzerCode <zlo.alien@gmail.com>
2024-03-24 21:20:35 +0200

    sidebar

--- components/content.js
+++ components/content.js
@@ -1523,8 +1523,55 @@ header(<span class="hljs-string" style="margin: 0px; padding: 0px; border: 0px; 
 																										<!-- Load Bludit Plugins: Page End -->
 																									</div>
 																									<hr>
-																								</div>
-																								<!-- Sidebar -->
-    <div id="sidebar" class="sidebar">
-					<script src="./components/sidebar.js"></script>
-	</div>`;
+																									<!-- Pagination -->
+																									</div>
+
+
+<!-- Right Sidebar -->
+<div class="col-md-3">
+
+<div class="plugin plugin-categories">
+ <h2 class="plugin-label">Categories</h2>
+ <div class="plugin-content">
+  <ul>
+   <li><a href="https://inforax.com/category/general">General (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/startup">Startup (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/ideas">Идеи (3)</a>
+   </li>
+   <li><a href="https://inforax.com/category/projects">Проекты (2)</a>
+   </li>
+   <li><a href="https://inforax.com/category/social">Соцсети (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/tasks">Задачки(6)</a>
+   </li>
+   <li><a href="https://inforax.com/category/telegramm">Telegramm (1)</a>
+   </li>
+   <li><a href="https://inforax.com/category/usefultools">Инструменты (21)</a>
+   </li>
+  </ul>
+ </div>
+</div>
+
+
+<div class="plugin plugin-navigation">
+ <h2 class="plugin-label">Latest</h2>
+ <div class="plugin-content">
+  <ul>
+   <li><a href="https://inforax.com/seed-prase">Основы криптографии? Что такое исходная фраза?</a></li>
+   <li><a href="https://inforax.com/generate-ssh-key-cpanel">Как создать ssh key в панели CPanel</a></li>
+   <li><a href="https://inforax.com/exim">Полезные действия Exim</a></li>
+   <li><a href="https://inforax.com/zip-ssh">ZIP SSH - как запаковать все файлы в текущем каталоге</a></li>
+   <li><a href="https://inforax.com/osnova-paradigmy">Основа парадигмы</a></li>
+  </ul>
+ </div>
+</div>
+
+<div class="plugin plugin-about">
+ <h2 class="plugin-label">Contact:</h2>
+ <div class="plugin-content">Tlg:@RedBlackTg</div>
+</div>
+
+</div>
+																									`;
